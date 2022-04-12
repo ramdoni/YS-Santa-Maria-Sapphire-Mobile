3e84242c6d3ae8d3d75c186a660c96d77a54c4ca
Ramdoni <doni.engine@gmail.com>
2022-04-12 23:22:16 +0700

    update: login form

--- app/Http/Livewire/Login.php
+++ app/Http/Livewire/Login.php
@@ -4,12 +4,14 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
+use App\Models\UserPlatinum;
+use Illuminate\Support\Str;
 
 class Login extends Component
 {
     public $email;
     public $password,$remember_me=false;
-    public $message;
+    public $message,$type_login=1;
     public function render()
     {
         return view('livewire.login')
@@ -26,16 +28,34 @@ class Login extends Component
                 'email.required'=>'NIK / No Anggota is required'
             ]
         );
+
         if(is_numeric($this->email)){ 
             $credentials = ['nik'=>$this->email,'password'=>$this->password]; // Login with NIK
         }else{
             $credentials = ['email'=>$this->email,'password'=>$this->password];
         }
 
-        if (Auth::attempt($credentials,$this->remember_me)) {
-            // Authentication passed...
-            return redirect('/');
+        if($this->type_login==1){
+            if (Auth::attempt($credentials,$this->remember_me)) {
+                // Authentication passed...
+                return redirect('/');
+            }else $this->message = __('Email / Password incorrect please try again');
+        }else{
+            if(is_numeric($this->email))
+                $user = UserPlatinum::where('nik',$this->email)->first();
+            else
+                $user = UserPlatinum::where('email',$this->email)->first();
+            
+            if (isset($user->name) and \Hash::check($this->password, $user->password)){
+                $tokenLogin = base64_encode(\Str::random(40));
+
+                $user->token_login = $tokenLogin;
+                $user->save();
+
+                return redirect(env('PLATINUM_URL')."/login-with-token/{$tokenLogin}");
+
+            }else $this->message = __('Email / Password incorrect please try again');
         }
-        else $this->message = __('Email / Password incorrect please try again');
+            
     }
 }
